dd7a11bf2922b4be05aae11046524aebb4f858fd
kintys <dima.sytnik92@gmail.com>
2024-02-15 22:28:05 +0200

    add new project

---
+++ barbershop_2/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterView, RouterLink } from 'vue-router'
+</script>
+
+<template>
+    <router-link :to="{ name: 'home' }">home</router-link>
+    <router-link :to="{ name: 'about' }">about</router-link>
+    <RouterView />
+</template>
+
+<style scoped></style>
---
+++ barbershop_2/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+    <main>
+        <div id="main" class="item">dasdadasdasdasdasdasd</div>
+    </main>
+</template>
+<style lang="scss">
+.item {
+    margin-top: 3000px;
+}
+</style>
