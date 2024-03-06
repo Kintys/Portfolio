aecab06d295420a8caea73dae016cd2210b642d1
kintys <dima.sytnik92@gmail.com>
2024-03-06 21:47:30 +0100

    add new component

--- barbershop_2/src/page/book-page/BookPageSection.vue
+++ barbershop_2/src/page/book-page/BookPageSection.vue
@@ -1,62 +1,73 @@
 <template>
     <div class="container">
-        <v-text-field
-            variant="outlined"
-            label="Your name"
-            placeholder="Name"
-            class="input"
-            :rules="nameRules.nameRules"
-            base-color="#000"
-            clearable
-            color="#000"
-            v-model="userName"
-            name="user_Name"
-        ></v-text-field>
-        <v-text-field
-            variant="outlined"
-            base-color="#000"
-            color="#000"
-            label="Your email"
-            :rules="nameRules.emailRules"
-            placeholder="Email"
-            class="input"
-            hide-details="auto"
-            clearable
-            v-model="email"
-            name="user_email"
-        ></v-text-field>
-
-        <v-row justify="center">
-            <v-date-picker
-                view-mode="month"
-                width="400"
-                v-model="data"
-                hide-header="true"
-            ></v-date-picker>
-        </v-row>
-        <VueTimepicker
-            hide-disabled-items
-            format="HH:mm "
-            :hour-range="[[9, 17]]"
-            :minute-range="[0, 15, 30, 45]"
-        />
+        <v-form ref="form" @submit.prevent="sendEmail">
+            <form class="from-template" ref="formTemplate">
+                <input type="text" name="user_name" :value="userName" />
+                <input type="email" name="user_email" :value="userEmail" />
+                <input type="data" name="user_data" :value="fullDateView" />
+                <input type="text" name="user_hour" :value="time?.HH" />
+                <input type="text" name="user_minute" :value="time?.mm" />
+            </form>
+            <v-text-field
+                variant="outlined"
+                label="Your name"
+                placeholder="Name"
+                class="input"
+                :rules="nameRules.nameRules"
+                base-color="#000"
+                clearable
+                color="#000"
+                v-model="userName"
+            ></v-text-field>
+            <v-text-field
+                variant="outlined"
+                base-color="#000"
+                color="#000"
+                label="Your email"
+                :rules="nameRules.emailRules"
+                placeholder="Email"
+                class="input"
+                hide-details="auto"
+                clearable
+                v-model="userEmail"
+            ></v-text-field>
+            <v-row justify="center">
+                <v-date-picker
+                    view-mode="month"
+                    width="400"
+                    v-model="userData"
+                    hide-header="true"
+                ></v-date-picker>
+            </v-row>
+            <VueTimepicker
+                class="timepicker"
+                v-model="time"
+                hide-disabled-items
+                format="HH:mm"
+                :hour-range="[[9, 17]]"
+                :minute-range="[0, 15, 30, 45]"
+            />
+        </v-form>
+        <v-btn @click="sendEmail">book</v-btn>
     </div>
-    {{ fullDateView }}
 </template>
 
 <script setup>
 import { ref, computed } from "vue";
 import { useDate } from "vuetify";
+import emailjs from "@emailjs/browser";
 import VueTimepicker from "vue3-timepicker";
-
 // CSS
-import "vue3-timepicker/dist/VueTimepicker.css";
+
+const time = ref(null);
 const dateConstructor = useDate();
 const userName = ref(null);
-const email = ref(null);
-const data = ref(null);
+const userEmail = ref(null);
+const userData = ref(null);
+const form = ref(null);
+const formTemplate = ref(null);
 const fullDateView = computed(() => {
-    return dateConstructor.format(data.value, "fullDateWithWeekday");
+    return dateConstructor.format(userData.value, "fullDateWithWeekday");
 });
 
 const validRegex =
@@ -70,11 +81,35 @@ const nameRules = ref({
         },
     ],
 });
+
+async function sendEmail() {
+    const { valid } = await form.value.validate();
+    if (valid) createForm();
+}
+
+function createForm() {
+    emailjs
+        .sendForm("service_jk5a3z3", "template_uojhabm", formTemplate.value, {
+            publicKey: "IgXLm12XkrQHhfgEt",
+        })
+        .then(
+            () => {
+                console.log("SUCCESS!");
+            },
+            (error) => {
+                console.log("FAILED...", error.text);
+            }
+        );
+}
 </script>
 
 <style lang="scss" scoped>
+@import "@/assets/style/lib/mixins";
 .container {
     background: #fff;
     padding-top: 100px;
+}
+.from-template {
+    display: none;
 }
 </style>
