57011b56bb0978740a18e5f6d24918ae7471868a
kintys <dima.sytnik92@gmail.com>
2024-12-16 23:26:05 +0100

    add filter methods

--- exclusive/src/App.vue
+++ exclusive/src/App.vue
@@ -3,6 +3,7 @@ import { onMounted } from 'vue'
 import { RouterView } from 'vue-router'
 import { useAuthStore } from '@/stores/auth'
 const { loginWithGoogleToken } = useAuthStore()
+
 onMounted(() => {
     loginWithGoogleToken()
 })
@@ -13,4 +14,3 @@ onMounted(() => {
 </template>
 
 <style scoped></style>
-@/stores/gamepad.js
